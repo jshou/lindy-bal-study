b4e7f2773f78534fbc513e4edb0ec6931fca001a
jshou <joshuahou@gmail.com>
2016-02-14 13:22:56 -0800

    tempo stuff

--- classifier.py
+++ classifier.py
@@ -45,20 +45,26 @@ if __name__ == '__main__':
     triplety = [row[1] == '1' for row in attribute_rows]
     backbeat = [row[2] == '1' for row in attribute_rows]
     accent_m = [row[3] == '1' for row in attribute_rows]
-    big_band = [row[4] == '1' for row in attribute_rows]
+    big_band = [row[4] == '0' for row in attribute_rows]
     crashy_c = [row[5] == '1' for row in attribute_rows]
 
-    for speed in range(130, 250, 10):
-        bal_speed(bal_gold, speeds, speed)
     print("bal triplety: " + str(accuracy(bal_gold, [not i for i in triplety])))
     print("bal backbeat: " + str(accuracy(bal_gold, [not i for i in backbeat])))
     print("bal accent_m: " + str(accuracy(bal_gold, [not i for i in accent_m])))
     print("bal big_band: " + str(accuracy(bal_gold, big_band)))
 
-
-    for speed in range(130, 250, 10):
-        lindy_speed(lindy_gold, speeds, speed)
     print("lindy triplety: " + str(accuracy(lindy_gold, triplety)))
     print("lindy backbeat: " + str(accuracy(lindy_gold, backbeat)))
     print("lindy accent_m: " + str(accuracy(lindy_gold, accent_m)))
     print("lindy big_band: " + str(accuracy(lindy_gold, big_band)))
+
+
+    print("bal tempos:")
+    for song_index, is_bal in enumerate(bal_gold):
+        if is_bal:
+            print(speeds[song_index])
+
+    print("lindy tempos:")
+    for song_index, is_lindy in enumerate(lindy_gold):
+        if is_lindy:
+            print(speeds[song_index])
